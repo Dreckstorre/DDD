55c0f5fb84112c292b3156633fe85b4832633b2a
Olivier Eisenkremer <olivier.eizen@gmail.com>
2020-04-24 17:50:15 +0200

    end of ddd : forgot the README

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Groupe 4 - Olivier EISENKREMER, Benoit PINAULT, Gregoire LAMY, Kyllian GAUTHIER
 
+### You must have node JS installed on the computer ###
+### In the root folder do in a terminal .. ###
 npm install
+### then for test & run ###
 ng test
--- src/app/use_case/entretien/plan-interview.spec.ts
+++ src/app/use_case/entretien/plan-interview.spec.ts
@@ -17,8 +17,4 @@ describe('PlanInterview', () => {
     expect(plannedInterview).toBeTruthy();
   });
 
-  it('should create an instance', () => {
-    plannedInterview.plan({candidateId : 0, interviewDay : new Date('2020-04-22T00:00:00')});
-    expect(plannedInterview).toBeTruthy();
-  });
 });
